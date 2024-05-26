f6a96f4db508d7a5e6ecb347295d2f4aa8e0e936
vittxr <vitor.roberto3022@gmail.com>
2024-05-26 11:09:52 -0300

    replace user image of user-dropdown with a generic-user-icon

--- react_frontend/src/screens/BaseScreen.tsx
+++ react_frontend/src/screens/BaseScreen.tsx
@@ -1,7 +1,7 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Fragment } from 'react'
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems, Transition } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, UserCircleIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { classNames } from '@/utils/tw'
 import { Link } from 'react-router-dom'
 
@@ -73,11 +73,7 @@ export default function BaseScreen({ children }: Props) {
                       <div>
                         <MenuButton className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                           <span className="sr-only">Open user menu</span>
-                          <img
-                            className="h-8 w-8 rounded-full"
-                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt=""
-                          />
+                          <UserCircleIcon className="h-8 w-8 text-white" aria-hidden="true" />
                         </MenuButton>
                       </div>
                       <Transition
