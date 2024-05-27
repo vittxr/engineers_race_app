972b20af96f2e2f1ba3df9edb81bad222ab009bb
vittxr <vitor.roberto3022@gmail.com>
2024-05-26 21:55:29 -0300

    update database tables structure.

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `tests`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(64) NOT NULL UNIQUE,
+    `name` ENUM("Subida de Rampa em 45° (contagem de distância)", "Velocidade máxima com manobrabilidade (contagem de tempo)", "Tração (contagem de peso)") UNIQUE,
     `race_score` INT NOT NULL,
     `race_position` INT NOT NULL,
     `value` DECIMAL(8, 2) NOT NULL,
-    `value_description` VARCHAR(64) NOT NULL,
+    `value_description` ENUM("contagem de distância", "contagem de tempo", "contagem de peso") NOT NULL,
     `penalty` DECIMAL(8, 2) NULL,
     `penalty_description` VARCHAR(64) NULL,
     `squad_id` INT UNSIGNED NOT NULL
@@ -22,6 +22,6 @@ CREATE TABLE `students`(
     `squad_id` INT UNSIGNED NOT NULL
 );
 ALTER TABLE
-    `students` ADD CONSTRAINT `students_squad_id_foreign` FOREIGN KEY(`squad_id`) REFERENCES `squads`(`id`);
+    `students` ADD CONSTRAINT `students_squad_id_foreign` FOREIGN KEY(`squad_id`) REFERENCES `squads`(`id`) ON DELETE CASCADE;
 ALTER TABLE
-    `tests` ADD CONSTRAINT `tests_squad_id_foreign` FOREIGN KEY(`squad_id`) REFERENCES `squads`(`id`);
+    `tests` ADD CONSTRAINT `tests_squad_id_foreign` FOREIGN KEY(`squad_id`) REFERENCES `squads`(`id`) ON DELETE CASCADE;
