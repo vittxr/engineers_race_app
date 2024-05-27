583f4fb19c91b74f0d52d126a0be2987d4ef8d5e
vittxr <vitor.roberto3022@gmail.com>
2024-05-26 21:55:12 -0300

    add get_squad() function

--- backend/database/crud/squads.py
+++ backend/database/crud/squads.py
@@ -1,15 +1,48 @@
+import json
+from typing import Any
 from database.crud import create_students
-from database.schemas import Squad, SquadCreate, SquadUpdate
+from database.crud.students import delete_students_from_squad
+from database.schemas import SquadCreate, SquadUpdate
+from database.schemas.students import Student
 from dependencies import connection
+from utils.error.error_messages import CANNOT_CREATE_SQUAD, SQUAD_NOT_FOUND
 
 
 async def get_squads():
     with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM squads")
+        cursor.execute("SELECT s.id, s.name, s.grade FROM squads AS s")
         return cursor.fetchall()
 
 
-async def create_squad(squad: SquadCreate):
+async def get_squad(id: int):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """SELECT 
+                s.id, 
+                s.name, 
+                s.car_id,
+                s.grade, 
+                (
+                    SELECT JSON_ARRAYAGG(JSON_OBJECT('id', st.id, 'name', st.name, 'RA', st.RA)) 
+                    FROM students AS st WHERE st.squad_id = s.id
+                ) as students 
+            FROM squads AS s WHERE id = %s""",
+            (id,),
+        )
+        data: dict[str, Any] | None = cursor.fetchone()
+        if data is None:
+            raise LookupError(SQUAD_NOT_FOUND)
+
+        students: list[Student] = (
+            json.loads(data["students"]) if data["students"] else []
+        )
+        return {
+            **data,
+            "students": students,
+        }
+
+
+async def create_squad(squad: SquadCreate) -> None:
     """
     Create a new squad.
 
@@ -23,10 +56,14 @@ async def create_squad(squad: SquadCreate):
         sql = "INSERT INTO squads (name, car_id) VALUES (%s, %s) RETURNING *"
         cursor.execute(sql, (squad.name, squad.car_id))
         db_squad = cursor.fetchone()
-        students = await (
-            create_students(squad.students, db_squad["id"]) if squad.students else []
-        )
-        return Squad(**db_squad, students=students)
+
+        if db_squad is None:
+            raise Exception(CANNOT_CREATE_SQUAD)
+
+        if squad.students:
+            await create_students(squad.students, db_squad["id"])
+
+        # return Squad(**db_squad, students=students)
 
 
 async def update_squad(id: int, squad: SquadUpdate) -> None:
@@ -40,13 +77,20 @@ async def update_squad(id: int, squad: SquadUpdate) -> None:
         None
     """
     with connection.cursor() as cursor:
-        squad = squad.model_dump(
+        dict_squad = squad.model_dump(
             exclude_none=True,
+            exclude={"students"},
         )
-        set_clause = ", ".join([f"{column} = %s" for column in squad.keys()])
+        set_clause = ", ".join([f"{column} = %s" for column in dict_squad.keys()])
         sql = f"UPDATE squads SET {set_clause} WHERE id = %s"
-        sql_data = tuple(squad.values())
+        sql_data = tuple(dict_squad.values())
+
         cursor.execute(sql, (*sql_data, id))
+
+        # this is npt a good solution, but it works. Here all students will be deleted from the squad and created again.
+        await delete_students_from_squad(id)
+        if squad.students:
+            await create_students(squad.students, id)
 
 
 async def delete_squad(id: int) -> None:
