d4cee0fc2866d382799dd47ecdba57550c97b411
vittxr <vitor.roberto3022@gmail.com>
2024-05-30 19:54:28 -0300

    add prettier

--- react_frontend/src/screens/tests/fragments/TestForm.tsx
+++ react_frontend/src/screens/tests/fragments/TestForm.tsx
@@ -1,4 +1,4 @@
-import RHForm from '@/components/Form'
+import RHForm from '@/components/Form';
 import { httpClient } from '@/utils/requests';
 import { Squad } from '@/utils/types/apiSchemas';
 import { TTestFormData } from '@/utils/zodSchemas/test';
@@ -10,53 +10,84 @@ type Props = {
   buttonText: string;
   test?: Squad;
   isLoading?: boolean;
-}
+};
 
 const TestForm = ({ mutation, buttonText, isLoading }: Props) => {
-  const [title, setTitle] = useState<string>('Criar registro de prova - Subida de Rampa em 45° (contagem de distância)')
-  const [valueUnit, setValueUnit] = useState<'metros' | 'segundos' | 'gramas'>('metros')
-  const squads = useQuery({ 
+  const [title, setTitle] = useState<string>(
+    'Criar registro de prova - Subida de Rampa em 45° (contagem de distância)',
+  );
+  const [valueUnit, setValueUnit] = useState<'metros' | 'segundos' | 'gramas'>(
+    'metros',
+  );
+  const squads = useQuery({
     queryKey: ['squads'],
-    queryFn: () => httpClient.get('/squads')
-  })
+    queryFn: () => httpClient.get('/squads'),
+  });
 
-  const handleTestTypeChange = async (value: string) => { 
-    setTitle(`Criar registro de prova - ${value}`)
-    switch(value) {
+  const handleTestTypeChange = async (value: string) => {
+    setTitle(`Criar registro de prova - ${value}`);
+    switch (value) {
       case 'Subida de Rampa em 45° (contagem de distância)':
         setValueUnit('metros');
-        break
+        break;
       case 'Velocidade máxima com manobrabilidade (contagem de tempo)':
-        setValueUnit('segundos')
-        break
+        setValueUnit('segundos');
+        break;
       case 'Tração (contagem de peso)':
-        setValueUnit('gramas')
-        break
+        setValueUnit('gramas');
+        break;
     }
-  }
+  };
 
   return (
     <>
-      <h1 className='font-medium text-2xl text-center'>{title}</h1>
-      <RHForm id="test-form" onSubmit={mutation.mutateAsync} buttonText={buttonText}>
-        <RHForm.Select label="Nome (tipo de prova)" id="name" 
+      <h1 className="font-medium text-2xl text-center">{title}</h1>
+      <RHForm
+        id="test-form"
+        onSubmit={mutation.mutateAsync}
+        buttonText={buttonText}
+      >
+        <RHForm.Select
+          label="Nome (tipo de prova)"
+          id="name"
           options={[
-            { value: 'Subida de Rampa em 45° (contagem de distância)', label: 'Subida de Rampa em 45° (contagem de distância)' },
-            { value: 'Velocidade máxima com manobrabilidade (contagem de tempo)', label: 'Velocidade máxima com manobrabilidade (contagem de tempo)' },
-            { value: 'Tração (contagem de peso)', label: 'Tração (contagem de peso)' },
+            {
+              value: 'Subida de Rampa em 45° (contagem de distância)',
+              label: 'Subida de Rampa em 45° (contagem de distância)',
+            },
+            {
+              value:
+                'Velocidade máxima com manobrabilidade (contagem de tempo)',
+              label:
+                'Velocidade máxima com manobrabilidade (contagem de tempo)',
+            },
+            {
+              value: 'Tração (contagem de peso)',
+              label: 'Tração (contagem de peso)',
+            },
           ]}
           onChange={handleTestTypeChange}
         />
-        <RHForm.Input type="number" label={`Valor - ${valueUnit}`}id="value" />
+        <RHForm.Input type="number" label={`Valor - ${valueUnit}`} id="value" />
         <RHForm.Input type="number" label="Penalidade" id="penalty" />
-        <RHForm.Input type="text" label="Descrição da penalidade" id="penalty_description" />
-        <RHForm.Select label="Equipe" id="squad_id" options={squads.data?.data?.map((s: Squad) => {
-          return { value: s.id, label: s.name }
-        }) || []} isLoading={isLoading} />
+        <RHForm.Input
+          type="text"
+          label="Descrição da penalidade"
+          id="penalty_description"
+        />
+        <RHForm.Select
+          label="Equipe"
+          id="squad_id"
+          options={
+            squads.data?.data?.map((s: Squad) => {
+              return { value: s.id, label: s.name };
+            }) || []
+          }
+          isLoading={isLoading}
+        />
       </RHForm>
-
     </>
-  )
-}
+  );
+};
 
-export default TestForm
+export default TestForm;
